8d481ad31ed5f111b3da50832058bd76b84b0a84
joeldba <75224242+joeldba@users.noreply.github.com>
2020-12-17 17:12:43 -0700

    changed color scheme from Nord to Hybrid

--- config.h
+++ config.h
@@ -99,30 +99,30 @@ float alpha = 0.85;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3b4252", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
+	"#282a2e", /* black   */
+	"#a54242", /* red     */
+	"#8c9440", /* green   */
+	"#de935f", /* yellow  */
+	"#5f819d", /* blue    */
+	"#85678f", /* magenta */
+	"#5e8d87", /* cyan    */
+	"#707880", /* white   */
 
 	/* 8 bright colors */
-     	"#4c566a", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */ 
+     	"#373b41", /* black   */
+	"#cc6666", /* red     */
+	"#b5bd68", /* green   */
+	"#f0c674", /* yellow  */
+	"#81a2be", /* blue    */
+	"#b294bb", /* magenta */
+	"#8abeb7", /* cyan    */
+	"#c5c8c6", /* white   */ 
 
 	[255] = 0,
 
  	/* more colors can be added after 255 to use with DefaultXX */
-	"#2e3440", /* background */
-	"#d8dee9", /* foreground */
+	"#1d1f21", /* background */
+	"#c5c8c6", /* foreground */
 	};
 
 
